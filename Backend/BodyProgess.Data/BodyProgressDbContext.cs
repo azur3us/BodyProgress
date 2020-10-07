15485b88c601eabd8e06e224a4765a6ddba126f1
Patryk Binicki <pbinicki@gmail.com>
2020-10-07 22:38:17 +0200

    Installed EF Core and created data base context class with name BodyProgressDbContext.

---
+++ Backend/BodyProgess.Data/BodyProgressDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BodyProgess.Data
+{
+    public class BodyProgressDbContext: DbContext
+    {
+        public BodyProgressDbContext(DbContextOptions<BodyProgressDbContext> options): base(options)
+        {
+
+        }
+    }
+}
