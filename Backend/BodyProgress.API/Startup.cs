58e3aaad46b7fd65269008d1528cc69b14a31278
Patryk Binicki <pbinicki@gmail.com>
2020-10-11 18:16:54 +0200

    Added automapper to the project.

--- Backend/BodyProgress.API/Startup.cs
+++ Backend/BodyProgress.API/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using BodyProgess.Data;
+using BodyProgress.API.AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +34,14 @@ namespace BodyProgress.API
             services.AddControllers();
 
             services.AddSwaggerGen();
+
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new AutoMapperProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
